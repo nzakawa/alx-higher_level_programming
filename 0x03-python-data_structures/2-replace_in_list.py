edebcc3792334124b75651b4bb886409915b7ffa
nzakawa <zakawa1@gmail.com>
2022-12-04 11:58:31 -0800

    task 0 and 1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,7 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx > (len(my_list) - 1) or idx < 0:
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list
+    """Replace an element of a list at a specific position."""
+     if idx >= 0 and idx < len(my_list):
+            my_list[idx] = element
+        return (my_list)
