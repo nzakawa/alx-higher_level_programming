067bd807ffd8605fe97eeeac946b103fff1e84a5
nzakawa <zakawa1@gmail.com>
2022-12-20 13:01:24 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-""" Square module """
+"""Define a class square."""
 
 
 class Square:
-     """ Declares a square class """
+     """Represent a square."""
 
-
-    def __init__(self, size) -> None:
-        """
-        Initializes class attributes
+    def __init__(self, size):
+        """Initialize a new square.
 
         Args:
-            size: size of square
+            size (int): The size of the new square.
         """
         self.__size = size
