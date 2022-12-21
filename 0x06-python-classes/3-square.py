c1d06939332df77e3ef888221af357d10314432e
nzakawa <zakawa1@gmail.com>
2022-12-20 16:21:22 -0800

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,14 +8,14 @@ class Square:
      Attributes:
         size: size of the square
     """
-     __size = 0
+    __size = 0
 
-     def __init__(self, prmSize=0):
-         if not isinstance(prmSize, int):
-             raise TypeError("size must be an integer")
-         if prmSize < 0:
-             raise ValueError("size must be >= 0")
-         self.__size = prmSize
+    def __init__(self, prmSize=0):
+        if not isinstance(prmSize, int):
+            raise TypeError("size must be an integer")
+        if prmSize < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = prmSize
 
-        def area(self):
-            return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
