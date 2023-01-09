353f7c2d85f2f9941865b4694485b0557f24811e
nzakawa <zakawa1@gmail.com>
2023-01-09 13:46:15 -0800

    hotfix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,11 +13,6 @@ class Square(Rectangle):
     def __init__(self, size):
         """Method for initialized the attrubutes"""
 
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-
-        def area(self):
-            """rectangle area"""
-
-            return self.__size ** 2
+        super().__init__(size, size)
